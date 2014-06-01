bf53e424d2dde62bb5f7daa6660d69fcb6cbbb63
Sergey Kozachenko <grievoushead@serg-mac.local>
2014-06-01 20:34:14 +0300

    added categories, posts models and post controller actions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'This is some sample text for new ror blog first'
+    @content_second = 'This is some sample text for new ror blog second '
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index' => 'home#view'
 
+
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
